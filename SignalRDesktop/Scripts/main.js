b352fa4eea5112938cf54c907e43ad65b7a95282
RaphaelFonseca <raphael@fonseca.tech>
2018-02-20 15:05:45 -0500

    added chat connection via angular controller

--- SignalRDesktop/Scripts/main.js
+++ SignalRDesktop/Scripts/main.js
@@ -13,10 +13,30 @@
         var vm = this;
         // place holder
         vm.btnWeight = _btnWeight;
+        vm.btnConnect = _btnConnect;
 
         function _btnWeight() {
             //Working
             console.log("doing something");
+        }
+        function _btnConnect() {
+            try {
+                var chat = $.connection.hello;
+
+                chat.client.helloClient = function (str) {
+                    alert(str);
+                }
+
+                $.connection.hub.start().done(function () {
+                    chat.server.helloSignalR();
+                });
+            }
+            catch
+            {
+                console.log("ERROR Connecting to hub");
+            }
+
+
         }
     }
 })();
